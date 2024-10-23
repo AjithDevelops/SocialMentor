3d5c6fc70da868d5b452262487cecb7daacfd50e
Ajith <ajithkumardevelops@gmail.com>
2024-10-23 09:50:57 +0530

    Header section

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -1,17 +1,24 @@
 import styles from '@/styles/style';
 import { arrowUp } from '@/public/assets';
 import Image from 'next/image';
-const GetStarted: React.FC = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+import "../app/gloal.css";
+
+interface GetStartedProps {
+  getStartedText: string;
+  startedText: string;
+}
+
+const GetStarted: React.FC<GetStartedProps> = ({ getStartedText, startedText }) => (
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer shining-bubble-animation`}>
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
-        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
-          <span className='text-gray-50'>Get</span>
+        <p className='font-poppins font-medium text-[18px] leading-[23px] animated-item'>
+          <span className='text-gray-50'>{getStartedText}</span>
         </p>
-        <Image src={arrowUp} alt="arrow" className='w-[23px] h-[23px] object-contain ml-2' />
+        <Image src={arrowUp} alt="arrow" className='w-[23px] h-[23px] object-contain ml-2 animated-item' />
       </div>
-      <p className='font-poppins font-medium text-[18px] leading-[23px]'>
-        <span className='text-gray-50'>Started</span>
+      <p className='font-poppins font-medium text-[18px] leading-[23px] animated-item'>
+        <span className='text-gray-50'>{startedText}</span>
       </p>
     </div>
   </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,23 +9,19 @@ const Hero: React.FC = () => (
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
-      <div className=" flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-        <Image src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraph} ml-2`}>
-          <span className="text-white">20%</span> Discount For{" "}
+      <div className="flex flex-row items-center py-[12px] px-8 bg-discount-gradient rounded-[20px] mb-4">
+        <Image src={discount} alt="discount" className="w-[48px] h-[48px]" />
+        <p className={`${styles.paragraph} ml-4`}>
+          <span className="text-yellow-500 font-bold animate-pulse">20%</span> Discount For{" "}
           <span className="text-white">1 Month</span> Account
         </p>
       </div>
       <div className="flex flex-row justify-between items-center w-full">
-        <h1 className={`flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] ${styles.animatedText} social-mentor-animation`}>
-           {/* <br className="sm:block hidden" />{" "} */}
-          <span className="text-gradient">Social Mentor</span>{" "}
+        <h1 className={`flex-1 font-poppins font-semibold ss:text-[80px] text-[60px] text-white ss:leading-[120px] leading-[90px] ${styles.animatedText} social-mentor-animation`}>
+          <span className="text-gradient">Social Mentor</span>
         </h1>
-        <div className="ss:flex hidden md:mr-4 mr-0">
-          <GetStarted />
-        </div>
       </div>
-      <h1 className="w-full font-poppins font-semibold ss:text-[24px] text-[20px] text-white ss:leading-[40px] leading-[30px]">
+      <h1 className="w-full font-poppins font-semibold ss:text-[28px] text-[24px] text-white ss:leading-[50px] leading-[40px]">
         <span className="animated-item">✦ &nbsp;Social Media Mastery</span>
         <br />
         <span className="animated-item">✦ &nbsp;Digital Marketing</span>
@@ -36,23 +32,26 @@ const Hero: React.FC = () => (
         <br />
         <span className="animated-item">✦ &nbsp;Data Analysis & Insights</span>
       </h1>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <p className={`${styles.paragraph} max-w-[470px] mt-10`}>
         We Empower Your Digital presence with Entertainment 
       </p>
     </div>
-    {/* <div className={`${styles.flexCenter} flex-1 flex md:my-0 my-10 relative`}>
-      <Image
-        src={robot}
-        alt="billings"
-        className="w-[100%] h-[100%] relative z-[5]"
-      />
+
+    <div className={`${styles.flexCenter} flex-1 flex flex-col mx-4 md:my-0 my-10 relative space-y-4`}>
+      <div className="flex justify-start w-full">
+        <div className="bubble-animation"><GetStarted getStartedText={"Our"} startedText={"Services"} /></div> {/* Wrap in a div for animation */}
+      </div>
+      <div className="flex justify-end w-full">
+        <div className="bubble-animation"><GetStarted getStartedText={"Viral in"} startedText={"Social Media"} /></div> {/* Wrap in a div for animation */}
+      </div>
+      <div className="flex justify-start">
+        <div className="bubble-animation"><GetStarted getStartedText={"Get"} startedText={"Entertained"} /></div> {/* Wrap in a div for animation */}
+      </div>
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-    </div> */}
-    <div className={`${styles.flexCenter} ss:hidden`}>
-      <GetStarted />
     </div>
   </section>
 );
+
 export default Hero;
